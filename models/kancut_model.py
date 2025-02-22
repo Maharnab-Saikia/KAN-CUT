f04f90ec079df940b9d0c190437b22172bd7413d
Maharnab Saikia <61657669+Maharnab-Saikia@users.noreply.github.com>
2025-02-22 21:19:18 +0530

    Update kancut_model.py

--- models/kancut_model.py
+++ models/kancut_model.py
@@ -129,8 +129,8 @@ class KANCUTModel(BaseModel):
         The option 'direction' can be used to swap domain A and domain B.
         """
         AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].unsqueeze(0).to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].unsqueeze(0).to(self.device)
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
